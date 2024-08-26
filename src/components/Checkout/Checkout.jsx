5530fad819dcbf88b5673f6df050cb2f4f99a584
imrdoctor <mhamedfekry0@gmail.com>
2024-08-26 08:14:52 +0300

    paymint redirect

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -46,7 +46,7 @@ export default function Checkout() {
         }
       } else {
         // API call for online payment
-        responseOnline = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${pymintid}?url=http://localhost:5173`, formValue, {
+        responseOnline = await axios.post(`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${pymintid}?url=http://https://imrdoctor.github.io/freshcart/`, formValue, {
           headers: {
             token: localStorage.getItem("userToken"),
           },
